637bc6b72c2666da8ca040debbc8038a48d41ccf
me001416 <me001416@gmail.com>
2024-11-25 09:49:45 +0800

    Version 002

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import json
+
+window = tk.Tk()
+window.title('三國志14 戰法')
+window.geometry('380x400')
+window.resizable(False, False)
+# window.iconbitmap('icon.ico')
+window.mainloop()
