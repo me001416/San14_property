eec7d6fd1d0783e7d94d836791cc2479836ac2d3
me001416 <me001416@gmail.com>
2024-11-25 11:43:20 +0800

    Version 007

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 import tkinter as tk
 import json
 
-window = tk.Tk()
-window.title('三國志14 戰法')
-window.geometry('380x400')
-window.resizable(False, False)
-# window.iconbitmap('icon.ico')
+def json_load():
+    with open('san14.json', 'r') as srcF:
+        srcJson = json.load(srcF)
 
-with open('san14.json', 'r') as srcF:
-    srcJson = json.load(srcF)
+    print(srcJson)
 
-print(srcJson)
+def json_data_generate():
+    san14_data = {
+        "戰法" : "奮戰",
+        "系統" : "攻擊",
+        "對應能力" : "武力",
+        "對據點" : "-",
+        "發動時間" : "12日",
+        "效果一" :
+        {
+            "效果" : "傷害",
+            "範圍" : 1,
+            "威力" : 10,
+            "時間" : 0
+        },
+        "效果二" : 
+        {
+            "效果" : "-",
+            "範圍" : 0,
+            "威力" : 0,
+            "時間" : 0
+        }
+    }
 
-# window.mainloop()
+if __name__ == '__main__':
+
+    window = tk.Tk()
+    window.title('三國志14 戰法')
+    window.geometry('380x400')
+    window.resizable(False, False)
+    # window.iconbitmap('icon.ico')
+
+    # json_load()
+    json_data_generate()
+
+
+    # window.mainloop()
