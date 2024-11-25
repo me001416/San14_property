0e78fbb8b86f710b6077a64a7d21aadefd5ef278
me001416 <me001416@gmail.com>
2024-11-25 11:24:32 +0800

    Version 003

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ window.title('三國志14 戰法')
 window.geometry('380x400')
 window.resizable(False, False)
 # window.iconbitmap('icon.ico')
-window.mainloop()
+
+with open('san14.json', 'r') as srcF:
+    srcJson = json.load(srcF)
+
+print(srcJson)
+
+# window.mainloop()
