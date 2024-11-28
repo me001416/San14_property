b7caecd01aea42da9dda7b90a1970bbe8d24f81f
me001416 <me001416@gmail.com>
2024-11-28 23:11:22 +0800

    Version 029

--- main.py
+++ main.py
@@ -30,18 +30,27 @@ def dict_parse(srcJson):
             # print(value1)
             # print(type(key1))
             # print(type(value1))
+            if key1 == '對據點':
+                continue
+
             if type(value1) is str :
                 temp_str += key1 + " : " + value1 + ' '
             if type(value1) is dict :
+                if value1['效果'] == 'N/A':
+                    temp_str += key1 + " : " + 'N/A '
+                    continue
+
                 for key2, value2 in value1.items() :
                     # print(key2)
                     # print(value2)
                     # print(type(key2))
                     # print(type(value2))
-                    if type(value2) is str :
-                        temp_str += key2 + " : " + value2
-                    if type(value2) is int :
-                        temp_str += key2 + " : " + str(value2)
+                    if key2 == '效果':
+                        temp_str += key1 + " : " + value2 + ' '
+                    elif type(value2) is str :
+                        temp_str += key2 + " : " + value2 + ' '
+                    elif type(value2) is int :
+                        temp_str += key2 + " : " + str(value2) + ' '
 
         result.append(temp_str)
 
@@ -96,6 +105,34 @@ def json_data_generate():
 
     with open("san14_test.json", 'w', encoding="utf-8") as f:
         json.dump(san14_data, f, indent=4, ensure_ascii=False)
+
+def create_window() -> tk.Tk:
+    """
+    Create and configure a Tkinter window for the game "三國志14 戰法".
+
+    This function initializes a Tkinter window with the specified title, dimensions, 
+    and resizing properties.
+
+    Returns:
+        tk.Tk: The configured Tkinter window.
+
+    Example:
+        window = create_game_window()
+        window.mainloop()
+    """
+    # 初始化 Tkinter 視窗
+    window = tk.Tk()
+    
+    # 設定視窗標題
+    window.title('三國志14 戰法')  # 設定遊戲名稱作為標題
+    
+    # 設定視窗尺寸
+    window.geometry('380x400')  # 寬 380, 高 400 像素
+    
+    # 設定視窗大小可調整
+    window.resizable(True, True)  # 允許水平與垂直調整
+    
+    return window
 
 if __name__ == '__main__':
 
