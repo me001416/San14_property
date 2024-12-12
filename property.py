cee5da634f54dc6243a0e7c74b588e8b04dd428d
Mark Hsu <markhsu2@amd.com>
2024-12-12 23:16:45 +0800

    Version 042

--- property.py
+++ property.py
@@ -1,12 +1,22 @@
+from San14_property.main import refresh_listbox
+
 class FlagManager:
-    def __init__(self):
-        self.flag_dict = {
+    def __init__( self ):
+        self.flag_list = {
             'show_damage': False,
             'enemy_morale_down': False,
             'chaos': False,
             'arson': False
         }
 
-    def toggle_button(self, src_button, flag):
+    def toggle_button( self, src_button, flag ):
         # 自定義按鈕切換邏輯
-        button_color(src_button, flag)
+        self.button_color( self, src_button, flag )
+
+    def button_color( self, src_button, flag ):
+        if flag:
+            src_button.config(bg='#FF4040')
+        else:
+            src_button.config(bg='#FFD39B')
+
+        refresh_listbox()
