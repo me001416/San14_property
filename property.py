cad89fd0633ecee11995f1a93b6e3930d9c01512
Mark Hsu <markhsu2@amd.com>
2024-12-12 23:51:23 +0800

    Version 045

--- property.py
+++ property.py
@@ -1,4 +1,5 @@
 from main import refresh_listbox
+import tkinter as tk
 
 class FlagManager:
     def __init__( self ):
@@ -8,6 +9,8 @@ class FlagManager:
             ['chaos', False, '混亂'],
             ['arson', False, '放火']
         ]
+        self.Json_list
+        self.new_Listbox
 
     def toggle_button( self, src_button, index ):
         # 自定義按鈕切換邏輯
@@ -19,7 +22,40 @@ class FlagManager:
         else:
             src_button.config(bg='#FFD39B')
 
-        refresh_listbox()
+        self.refresh_listbox()
 
     def return_value( self, index ):
         return self.flag_list[index][1], self.flag_list[index][2]
+
+    def set_json_list( self, src ):
+        self.Json_list = src
+
+    def set_new_Listbox( self, src ):
+        self.new_Listbox = src
+
+    def refresh_listbox( self ):
+
+        filtered_items = []
+
+        for line in self.Json_list:
+            for item in self.flag_list:
+                if item[2] in line and not item[1]
+            if "[傷害]" in line and not show_damage:
+                continue
+
+            if "[敵方士氣降低]" in line and not enemy_morale_down:
+                continue
+
+            if "[混亂]" in line and not chaos:
+                continue
+
+            if "[放火]" in line and not arson:
+                continue
+
+            filtered_items.append( line )
+
+        # Clear the Listbox first
+        self.new_Listbox.delete( 0, tk.END )
+
+        for item in filtered_items:
+            self.new_Listbox.insert( "end", item )
